e290eeb5fc028a4409d20451e3431baa4d63a32c
vin.xyz <vinzelmarano@gmail.com>
2025-08-13 12:39:39 +0800

    UPDATE: i put connection for database to use for backend data integration

--- salarydatabase.php
+++ salarydatabase.php
@@ -1,7 +1,13 @@
 <?php
-
+$localhost = 'localhost';
+$username = 'root';
+$password = '';
 $db_name = 'salary_database';
 
+$conn = new mysqli($localhost, $username, $password, $db_name);
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 ?>
